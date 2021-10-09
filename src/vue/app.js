398d161d2b763fd24078aaa93571c5df9ea9bd60
Ragnar Helgi <ragnarhelgi22@gmail.com>
2021-10-09 19:48:51 +0000

    localstorage

--- src/vue/app.js
+++ src/vue/app.js
@@ -1,14 +1,40 @@
+let clear_local = () => { // Clears local (Only for production) 
+    localStorage.clear();
+    window.location.reload(true);
+}
+
 let app = new Vue ({
+    /* - element id - */
     el: '#app',
-    
+
+    /* - variables of the app - */
     data: {
+        exercises: [],
+        muscle_groups: [],
 
+        user_schedule: localStorage.user_schedule || {
+            monday: [],
+            tuesday: [],
+            wednesday: [],
+            thursday: [],
+            friday: [],
+            saturday: [],
+            sunday: []
+        }
     },
 
-    mounted: {
-
+    /* - on mounting in start up cycle (see vue docs) - */
+    mounted: function () {
+        // fetches data from json and saves it
+        fetch('src/data/workout.json')
+            .then(r => r.json())
+            .then(data => {
+                this.exercises = data['exercises'];
+                this.muscle_groups = data['muscle_groups'];
+        })
     },
 
+    /* - functions that the program uses - */
     methods: {
 
     }
